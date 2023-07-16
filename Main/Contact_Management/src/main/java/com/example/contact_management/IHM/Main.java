be6bfc896648d9ed7e34bf6756c225a969d861f6
AymaneMG <aymanemaghouti16@gmail.com>
2023-07-16 11:19:56 +0100

    ready to run it

--- Main/Contact_Management/src/main/java/com/example/contact_management/IHM/Main.java
+++ Main/Contact_Management/src/main/java/com/example/contact_management/IHM/Main.java
@@ -1,24 +1,10 @@
 package com.example.contact_management.IHM;
 
-import com.example.contact_management.DataBase.AppInstaller;
-import com.example.contact_management.DataBase.DataBaseException;
-
-import java.io.IOException;
-
 public class Main {
 
 
     public static void main(String[] args) throws Exception {
-        try {
-            if (AppInstaller.checkIfAlreadyInstalled()) {
-                System.out.println("la base de donnees est deja exsite");
-            } else {
-                AppInstaller.run();
-                System.out.println("la base de donnes a ete bien cree");
-            }
-        } catch (Exception e) {
-            throw new DataBaseException(e);
-        }
+        System.out.println("hello java developers !");
     }
 
 }
